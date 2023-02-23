6082d9d156040ba2469fc8a7e646c1785dab77bd
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2023-02-23 15:48:51 +0200

    const OFFLINE_VERSION = 8;

--- src/official-website/CoinGardenWorld.Web/wwwroot/service-worker.js
+++ src/official-website/CoinGardenWorld.Web/wwwroot/service-worker.js
@@ -17,8 +17,7 @@ Copyright 2015, 2019 Google Inc. All Rights Reserved.
 
 // Incrementing OFFLINE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const OFFLINE_VERSION = 7;
-const CACHE_NAME = 'offline';
+const OFFLINE_VERSION = 8;
 // Customize this with a different URL if needed.
 //const OFFLINE_URL = 'offline.html';
 
--- src/official-website/CoinGardenWorld.Web/wwwroot/service-worker.published.js
+++ src/official-website/CoinGardenWorld.Web/wwwroot/service-worker.published.js
@@ -1,6 +1,8 @@
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
+const OFFLINE_VERSION = 9;
+
 self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
