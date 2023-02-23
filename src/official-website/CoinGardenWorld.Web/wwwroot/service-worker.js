36be5d9f4292099feaee110b77d340ce1bb32df5
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2023-02-23 12:26:16 +0200

    cleaned service worker

--- src/official-website/CoinGardenWorld.Web/wwwroot/service-worker.js
+++ src/official-website/CoinGardenWorld.Web/wwwroot/service-worker.js
@@ -22,46 +22,46 @@ const CACHE_NAME = 'offline';
 // Customize this with a different URL if needed.
 //const OFFLINE_URL = 'offline.html';
 
-self.addEventListener('activate', (event) => {
-    event.waitUntil((async () => {
-        // Enable navigation preload if it's supported.
-        // See https://developers.google.com/web/updates/2017/02/navigation-preload
-        if ('navigationPreload' in self.registration) {
-            await self.registration.navigationPreload.enable();
-        }
-    })());
+//self.addEventListener('activate', (event) => {
+//    event.waitUntil((async () => {
+//        // Enable navigation preload if it's supported.
+//        // See https://developers.google.com/web/updates/2017/02/navigation-preload
+//        if ('navigationPreload' in self.registration) {
+//            await self.registration.navigationPreload.enable();
+//        }
+//    })());
 
-    // Tell the active service worker to take control of the page immediately.
-    self.clients.claim();
-});
+//    // Tell the active service worker to take control of the page immediately.
+//    self.clients.claim();
+//});
 
-self.addEventListener('fetch', (event) => {
-    // We only want to call event.respondWith() if this is a navigation request
-    // for an HTML page.
-    if (event.request.mode === 'navigate') {
-        event.respondWith((async () => {
-            try {
-                // First, try to use the navigation preload response if it's supported.
-                const preloadResponse = await event.preloadResponse;
-                if (preloadResponse) {
-                    return preloadResponse;
-                }
+//self.addEventListener('fetch', (event) => {
+//    // We only want to call event.respondWith() if this is a navigation request
+//    // for an HTML page.
+//    if (event.request.mode === 'navigate') {
+//        event.respondWith((async () => {
+//            try {
+//                // First, try to use the navigation preload response if it's supported.
+//                const preloadResponse = await event.preloadResponse;
+//                if (preloadResponse) {
+//                    return preloadResponse;
+//                }
 
-                const networkResponse = await fetch(event.request);
-                return networkResponse;
-            } catch (error) {
-                // catch is only triggered if an exception is thrown, which is likely
-                // due to a network error.
-                // If fetch() returns a valid HTTP response with a response code in
-                // the 4xx or 5xx range, the catch() will NOT be called.
-                console.log('Fetch failed; returning offline page instead.', error);
+//                const networkResponse = await fetch(event.request);
+//                return networkResponse;
+//            } catch (error) {
+//                // catch is only triggered if an exception is thrown, which is likely
+//                // due to a network error.
+//                // If fetch() returns a valid HTTP response with a response code in
+//                // the 4xx or 5xx range, the catch() will NOT be called.
+//                console.log('Fetch failed; returning offline page instead.', error);
 
-                const cache = await caches.open(CACHE_NAME);
-                const cachedResponse = await cache.match(OFFLINE_URL);
-                return cachedResponse;
-            }
-        })());
-    }
+//                const cache = await caches.open(CACHE_NAME);
+//                const cachedResponse = await cache.match(OFFLINE_URL);
+//                return cachedResponse;
+//            }
+//        })());
+//    }
 
     // If our if() condition is false, then this fetch handler won't intercept the
     // request. If there are any other fetch handlers registered, they will get a
