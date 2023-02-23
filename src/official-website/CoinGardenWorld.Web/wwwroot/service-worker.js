a1e8404fef55b50dfeb6df1bfd2ff2bee64dafa7
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2023-02-23 15:51:12 +0200

    self.addEventListener('fetch', () => { });

--- src/official-website/CoinGardenWorld.Web/wwwroot/service-worker.js
+++ src/official-website/CoinGardenWorld.Web/wwwroot/service-worker.js
@@ -1,7 +1,7 @@
 // In development, always fetch from the network and do not enable offline support.
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
-//self.addEventListener('fetch', () => { });
+self.addEventListener('fetch', () => { });
 /*
 Copyright 2015, 2019 Google Inc. All Rights Reserved.
  Licensed under the Apache License, Version 2.0 (the "License");
