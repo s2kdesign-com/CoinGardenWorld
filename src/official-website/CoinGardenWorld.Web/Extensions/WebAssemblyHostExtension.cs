edeb99f40ad765ca7de6aa483d92331dd5435f3e
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2023-02-23 02:40:03 +0200

    Updated first time loading and translatons

--- src/official-website/CoinGardenWorld.Web/Extensions/WebAssemblyHostExtension.cs
+++ src/official-website/CoinGardenWorld.Web/Extensions/WebAssemblyHostExtension.cs
@@ -1,31 +1,50 @@
-﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.JSInterop;
 using System.Globalization;
+using System.Web;
 
 namespace CoinGardenWorld.Web.Extensions
 {
     public static class WebAssemblyHostExtension
     {
+        private static string _queryParamName = "language";
+
         public async static Task SetDefaultCulture(this WebAssemblyHost host)
-        {      
+        {
             var js = host.Services.GetRequiredService<IJSRuntime>();
+            var navigation = host.Services.GetRequiredService<NavigationManager>();
+
+
+            var query = HttpUtility.ParseQueryString(new Uri(navigation.Uri).Query);
+
+            var cultureInfo = CultureInfo.GetCultures(CultureTypes.NeutralCultures)
+                .ToList().FirstOrDefault(c => c.TwoLetterISOLanguageName == query[_queryParamName]);
 
             CultureInfo culture;
-            var browserLanguage = await js.InvokeAsync<string>("getBrowserLanguage");
-            var blazorCulture = await js.InvokeAsync<string>("blazorCulture.get");
-
-            if (!string.IsNullOrEmpty(blazorCulture))
+            if (cultureInfo != null && CultureInfo.CurrentCulture.TwoLetterISOLanguageName != cultureInfo.TwoLetterISOLanguageName)
             {
-                culture = new CultureInfo(blazorCulture);
+                culture = cultureInfo;
             }
             else
             {
-                culture = new CultureInfo(browserLanguage);
-                await js.InvokeVoidAsync("blazorCulture.set", browserLanguage);
+                var browserLanguage = await js.InvokeAsync<string>("getBrowserLanguage");
+                var blazorCulture = await js.InvokeAsync<string>("blazorCulture.get");
+
+                if (!string.IsNullOrEmpty(blazorCulture))
+                {
+                    culture = new CultureInfo(blazorCulture);
+                }
+                else
+                {
+                    culture = new CultureInfo(browserLanguage);
+                    await js.InvokeVoidAsync("blazorCulture.set", browserLanguage);
+                }
             }
 
             CultureInfo.DefaultThreadCurrentCulture = culture;
             CultureInfo.DefaultThreadCurrentUICulture = culture;
+
         }
     }
 }
