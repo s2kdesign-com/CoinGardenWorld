65836ed8a7f329e8c61df29454875cb76851cce7
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2023-03-04 16:12:44 +0200

    updated api for the bot

---
+++ src/garden-bot/CoinGardenBotCore.Api/Models/DTO/RecognizedFlowerModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinGardenBotCore_Api.Models.DTO {
+    public class RecognizedFlowerModel {
+        public string Name { get; set; }
+
+        public string Region { get; set; }
+
+        public decimal Probability { get; set; }
+    }
+}
--- src/garden-bot/CoinGardenBotCore.Api/Models/Responses/RecognizeFlowerByNameResponse.cs
+++ src/garden-bot/CoinGardenBotCore.Api/Models/Responses/RecognizeFlowerByNameResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoinGardenBotCore_Api.Models.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,6 @@ using System.Threading.Tasks;
 
 namespace CoinGardenBotCore_Api.Models.Responses {
     public class RecognizeFlowerByNameResponse {
+        public List<RecognizedFlowerModel> RecognizedFlowers { get; set; }
     }
 }
