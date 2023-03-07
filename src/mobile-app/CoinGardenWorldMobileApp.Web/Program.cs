fb89d3e77d70d22fe6e9564bffd6ba5958270074
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2023-03-07 13:33:15 +0200

    Updated layout for mobile app and added api version

--- src/mobile-app/CoinGardenWorldMobileApp.Web/Program.cs
+++ src/mobile-app/CoinGardenWorldMobileApp.Web/Program.cs
@@ -6,7 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetValue<string>("Api_Url")) });
 
 builder.Services.AddMsalAuthentication(options => {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
