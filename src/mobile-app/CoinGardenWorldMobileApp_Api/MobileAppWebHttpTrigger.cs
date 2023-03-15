1fffd4cb966c5c756f8092f747967b0257f9b041
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2023-03-15 02:54:04 +0200

    fixed build

--- src/mobile-app/CoinGardenWorldMobileApp_Api/MobileAppWebHttpTrigger.cs
+++ src/mobile-app/CoinGardenWorldMobileApp_Api/MobileAppWebHttpTrigger.cs
@@ -52,7 +52,6 @@ namespace CoinGardenWorldMobileApp_Api {
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            var userName = principal.Claims.First(c => c.Type == "name").Value;
             var userName = principal.Claims.FirstOrDefault(c => c.Type == "name")?.Value;
             response.WriteString($"{userName} - Welcome to Azure Functions!");
 
