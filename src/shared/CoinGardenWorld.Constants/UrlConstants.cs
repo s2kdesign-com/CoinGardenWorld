6ab8b6b09a013b5489ca9d8cd03cfc080fabdc5c
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2023-03-16 23:46:32 +0200

    Changed azure static web app config to theme config , lets see what will happen

---
+++ src/shared/CoinGardenWorld.Constants/UrlConstants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinGardenWorld.Constants {
+    public static class UrlConstants
+    {
+        public const string OfficialSite = "https://coingarden.world/";
+
+        public const string GardenBotSite = "https://bot.coingarden.world/";
+
+        public const string MobileAppSite = "https://app.coingarden.world/";
+
+        public const string MetaverseSite = "https://metaverse.coingarden.world/";
+
+        public const string CharitySite = "https://charity.coingarden.world/";
+
+        public const string NftStoreSite = "https://store.coingarden.world/";
+
+        public const string TokenSite = "https://token.coingarden.world/";
+
+
+    }
+}
