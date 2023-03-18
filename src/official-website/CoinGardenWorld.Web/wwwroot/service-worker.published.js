528217ab0c9c9797c3405714818167f6788ae696
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2023-03-18 17:18:58 +0200

    Updated offline version on published site to refresh the browser from the service worker

--- src/official-website/CoinGardenWorld.Web/wwwroot/service-worker.published.js
+++ src/official-website/CoinGardenWorld.Web/wwwroot/service-worker.published.js
@@ -1,7 +1,7 @@
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
-const OFFLINE_VERSION = 9;
+const OFFLINE_VERSION = 10;
 
 self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
