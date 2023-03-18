2e3cf776c5b2fd15c2be40c4ae3a8fe133d13f64
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2023-03-18 17:00:46 +0200

    // Incrementing OFFLINE_VERSION will kick off the install event and force // previously cached resources to be updated from the network. const OFFLINE_VERSION = 8;

--- src/mobile-app/CoinGardenWorldMobileApp.Web/wwwroot/service-worker.js
+++ src/mobile-app/CoinGardenWorldMobileApp.Web/wwwroot/service-worker.js
@@ -2,3 +2,7 @@
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
 self.addEventListener('fetch', () => { });
+
+// Incrementing OFFLINE_VERSION will kick off the install event and force
+// previously cached resources to be updated from the network.
+const OFFLINE_VERSION = 8;
