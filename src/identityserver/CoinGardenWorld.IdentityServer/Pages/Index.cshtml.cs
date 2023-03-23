ff011b62b5debc52356f4d908477d796dd575289
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2023-03-23 22:03:42 +0200

    #11 changed identity server template

--- src/identityserver/CoinGardenWorld.IdentityServer/Pages/Index.cshtml.cs
+++ src/identityserver/CoinGardenWorld.IdentityServer/Pages/Index.cshtml.cs
@@ -8,9 +8,15 @@ namespace CoinGardenWorld.IdentityServer.Pages.Home;
 public class Index : PageModel
 {
     public string Version;
+    public string ProductVersion;
         
     public void OnGet()
     {
         Version = typeof(Duende.IdentityServer.Hosting.IdentityServerMiddleware).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion.Split('+').First();
+
+
+        var productVersion = Assembly.GetExecutingAssembly().GetName().Version;
+        if (productVersion != null)
+            ProductVersion = productVersion.ToString();
     }
 }
