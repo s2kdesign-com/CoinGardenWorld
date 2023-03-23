3ae5398e50fb40299bf542b4f73b90c1a731a7b4
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2023-03-24 01:16:35 +0200

    #11 added automatic db migrations

--- src/identityserver/CoinGardenWorld.IdentityServer/Program.cs
+++ src/identityserver/CoinGardenWorld.IdentityServer/Program.cs
@@ -7,8 +7,7 @@ Log.Logger = new LoggerConfiguration()
 
 Log.Information("Starting up");
 
-try
-{
+try {
     var builder = WebApplication.CreateBuilder(args);
 
     builder.Host.UseSerilog((ctx, lc) => lc
@@ -20,25 +19,21 @@ try
         .ConfigureServices()
         .ConfigurePipeline();
 
-    // this seeding is only for the template to bootstrap the DB and users.
-    // in production you will likely want a different approach.
-#if DEBUG
+    Log.Information("Seeding database...");
     if (args.Contains("/seed"))
-    {
-        Log.Information("Seeding database...");
-        SeedData.EnsureSeedData(app);
-        Log.Information("Done seeding database. Exiting.");
-        return;
-    }
-#endif
+        SeedData.MigrateDatabase(app, true);
+    else
+        SeedData.MigrateDatabase(app);
+
+    Log.Information("Done seeding database. Exiting.");
+
     app.Run();
 }
 catch (Exception ex) when (ex.GetType().Name is not "StopTheHostException") // https://github.com/dotnet/runtime/issues/60600
 {
     Log.Fatal(ex, "Unhandled exception");
 }
-finally
-{
+finally {
     Log.Information("Shut down complete");
     Log.CloseAndFlush();
 }
--- src/identityserver/CoinGardenWorld.IdentityServer/SeedData.cs
+++ src/identityserver/CoinGardenWorld.IdentityServer/SeedData.cs
@@ -6,69 +6,57 @@ using Duende.IdentityServer.Models;
 
 namespace CoinGardenWorld.IdentityServer;
 
-public class SeedData
-{
-    public static void EnsureSeedData(WebApplication app)
-    {
-        using (var scope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
-        {
-            scope.ServiceProvider.GetService<PersistedGrantDbContext>().Database.Migrate();
+public class SeedData {
+    public static void MigrateDatabase(WebApplication app, bool addTestData = false) {
+        using (var scope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope()) {
+            scope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
 
-            var context = scope.ServiceProvider.GetService<ConfigurationDbContext>();
+            var context = scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
             context.Database.Migrate();
-            EnsureSeedData(context);
+
+            // Even if addTestData=true we will not populate the data if there is existing rows in the tables
+            if (addTestData)
+                EnsureSeedData(context);
         }
     }
 
-    private static void EnsureSeedData(ConfigurationDbContext context)
-    {
-        if (!context.Clients.Any())
-        {
+    private static void EnsureSeedData(ConfigurationDbContext context) {
+        if (!context.Clients.Any()) {
             Log.Debug("Clients being populated");
-            foreach (var client in Config.Clients.ToList())
-            {
+            foreach (var client in Config.Clients.ToList()) {
                 context.Clients.Add(client.ToEntity());
             }
             context.SaveChanges();
         }
-        else
-        {
+        else {
             Log.Debug("Clients already populated");
         }
 
-        if (!context.IdentityResources.Any())
-        {
+        if (!context.IdentityResources.Any()) {
             Log.Debug("IdentityResources being populated");
-            foreach (var resource in Config.IdentityResources.ToList())
-            {
+            foreach (var resource in Config.IdentityResources.ToList()) {
                 context.IdentityResources.Add(resource.ToEntity());
             }
             context.SaveChanges();
         }
-        else
-        {
+        else {
             Log.Debug("IdentityResources already populated");
         }
 
-        if (!context.ApiScopes.Any())
-        {
+        if (!context.ApiScopes.Any()) {
             Log.Debug("ApiScopes being populated");
-            foreach (var resource in Config.ApiScopes.ToList())
-            {
+            foreach (var resource in Config.ApiScopes.ToList()) {
                 context.ApiScopes.Add(resource.ToEntity());
             }
             context.SaveChanges();
         }
-        else
-        {
+        else {
             Log.Debug("ApiScopes already populated");
         }
 
-        if (!context.IdentityProviders.Any())
-        {
+        if (!context.IdentityProviders.Any()) {
             Log.Debug("OIDC IdentityProviders being populated");
-            context.IdentityProviders.Add(new OidcProvider
-            {
+            context.IdentityProviders.Add(new OidcProvider {
                 Scheme = "demoidsrv",
                 DisplayName = "IdentityServer",
                 Authority = "https://demo.duendesoftware.com",
@@ -76,8 +64,7 @@ public class SeedData
             }.ToEntity());
             context.SaveChanges();
         }
-        else
-        {
+        else {
             Log.Debug("OIDC IdentityProviders already populated");
         }
     }
