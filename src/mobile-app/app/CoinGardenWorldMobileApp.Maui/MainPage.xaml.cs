02e5cae36414d10c18e9e7a45b5dfe7e86944d4f
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2023-09-15 23:19:06 +0300

    Added .net 8 MAUI blazor hybrid for mobile app and blazor wasm for web and github action for build

---
+++ src/mobile-app/app/CoinGardenWorldMobileApp.Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CoinGardenWorldMobileApp.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/mobile-app/app/CoinGardenWorldMobileApp.WebApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using CoinGardenWorldMobileApp.WebApp;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+});
+
+await builder.Build().RunAsync();
