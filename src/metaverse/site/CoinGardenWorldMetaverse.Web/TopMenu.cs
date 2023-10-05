29a6693ffd74eade1f44a5d2daec8cec30c34fb1
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2023-10-05 17:59:15 +0300

    #13 Added Metaverse site and api

---
+++ src/metaverse/site/CoinGardenWorldMetaverse.Web/TopMenu.cs
@@ -0,0 +1,44 @@
+﻿using CoinGardenWorld.Theme.Models.Shared;
+using CoinGardenWorldMetaverse.Web.Localization;
+
+namespace CoinGardenWorldMetaverse.Web {
+    public class TopMenu : ITopMenu {
+        public TopMenu() {
+
+            MenuItems = new List<MenuItem>
+            {
+                new MenuItem
+                {
+                    Title = TopMenu_Resource.Home,
+                    Url = "#header"
+                },
+                new MenuItem
+                {
+                    Title = TopMenu_Resource.Information,
+                    SubMenuItems = new List<MenuItem>
+                    {
+                        new MenuItem
+                        {
+                            Title = TopMenu_Resource.Documentation,
+                            Url = CoinGardenWorld.Constants.UrlConstants.Documentation,
+                            IsExternalUrl = true
+                        },
+                        new MenuItem
+                        {
+                            Title = TopMenu_Resource.Github,
+                            Url = CoinGardenWorld.Constants.UrlConstants.GithubRepo,
+                            IsExternalUrl = true
+                        },
+                        new MenuItem
+                        {
+                            Title = TopMenu_Resource.Litepaper,
+                            Url = "#"
+                        }
+                    }
+                }
+            };
+        }
+
+        public List<MenuItem> MenuItems { get; set; }
+    }
+}
