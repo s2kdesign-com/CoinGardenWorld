3e4935a94b8fe4ed1baec5102657155cb1e0a2d7
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2023-10-05 15:31:23 +0300

    #6 Added Charities Site

---
+++ src/charities/site/CoinGardenWorldCharities.Web/Program.cs
@@ -0,0 +1,27 @@
+using CoinGardenWorld.Theme.Extensions;
+using CoinGardenWorld.Theme.Models.Shared;
+using CoinGardenWorldCharities.Web;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// Add Singleton with the top menu implementation 
+// TODO: Move this to Theme library - you can scan the entry assembly for classes that implement ITopMenu
+builder.Services.AddSingleton<ITopMenu, TopMenu>();
+
+// CoinGardenWorld.Theme
+await builder.AddCgwHttpClientExtensions();
+
+
+// CoinGardenWorld.Theme
+builder.Services.AddCgwThemeExtensions();
+
+var host = builder.Build();
+
+// Extension method
+await host.SetDefaultCulture();
+
+await builder.Build().RunAsync();
