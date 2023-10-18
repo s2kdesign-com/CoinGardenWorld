03c13986454b2cf6d31b8d80547527a7f64d8707
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2023-10-18 17:37:53 +0300

    #7 updated service published ${CACHE_VERSION}

--- src/mobile-app/app/CoinGardenWorldMobileApp.WebApp/wwwroot/service-worker.published.js
+++ src/mobile-app/app/CoinGardenWorldMobileApp.WebApp/wwwroot/service-worker.published.js
@@ -13,7 +13,7 @@ self.addEventListener('message', event => {
 const CACHE_VERSION = '1';
 
 const cacheNamePrefix = 'offline-cache-';
-const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
+const cacheName = `${cacheNamePrefix}${CACHE_VERSION}`;
 const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
 const offlineAssetsExclude = [ /^service-worker\.js$/ ];
 
