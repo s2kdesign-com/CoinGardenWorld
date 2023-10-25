833e7d7833c3f03f9810b96d51d0945b592f4db9
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2023-10-25 23:21:37 +0300

    #7 small refactoring

---
+++ src/garden-bot/bot/CoinGardenWorldBot_Api/Controllers/VersionController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using System.Security.Principal;
+
+namespace CoinGardenWorldBot_Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+
+        // GET: api/Version
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            Version version = Assembly.GetExecutingAssembly()?.GetName().Version ?? new Version(1, 0, 0);
+            return version.ToString();
+        }
+        // GET: api/AuthorizedVersion
+        [HttpGet]
+        [Authorize]
+        [Route("/[controller]/[action]")]
+        public ActionResult<string> GetAuthorized()
+        {
+
+            var userName = HttpContext.User.Claims.FirstOrDefault(c => c.Type == "name")?.Value;
+            return userName + " - Welcome to .Net Api!" ;
+        }
+    }
+}
