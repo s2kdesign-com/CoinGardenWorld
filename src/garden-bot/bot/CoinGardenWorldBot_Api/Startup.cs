2ced0e1f56b5d3083708b1d5f29679514b231d08
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2023-10-27 15:13:32 +0300

    #7 Added swagger with oauth2 authorization

--- src/garden-bot/bot/CoinGardenWorldBot_Api/Startup.cs
+++ src/garden-bot/bot/CoinGardenWorldBot_Api/Startup.cs
@@ -13,6 +13,11 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
+using System.IO;
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 
 namespace CoinGardenWorldBot_Api
 {
@@ -29,7 +34,7 @@ namespace CoinGardenWorldBot_Api
         public void ConfigureServices(IServiceCollection services)
         {
 
-            var healthTags = new List<string> { "bot","messages", "api" };
+            var healthTags = new List<string> { "bot", "messages", "api" };
             services.AddHealthChecks()
                 // Self
                 .AddCheck("https://coingardenbotcore.azurewebsites.net/", () => HealthCheckResult.Healthy("Build Version: " + Assembly.GetExecutingAssembly().GetName().Version), healthTags)
@@ -43,9 +48,86 @@ namespace CoinGardenWorldBot_Api
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddMicrosoftIdentityWebApi(azureAdConfig);
 
-            services.AddControllers().AddNewtonsoftJson(options => {
+            services.AddControllers().AddNewtonsoftJson(options =>
+            {
                 options.SerializerSettings.MaxDepth = HttpHelper.BotMessageSerializerSettings.MaxDepth;
             });
+
+            #region AddSwagger
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = $"{Assembly.GetExecutingAssembly().GetName().Version}",
+                    Title = "CGW - GardenBOT API",
+                    Description = "Backend service API for all external functions of CoinGarden GardenBOT API.",
+                    TermsOfService = new Uri("https://coingarden.world/terms-and-conditions"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "admin@CoinGarden.World",
+                        Url = new Uri("http://coingarden.world")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "GNU General Public License v3.0",
+                        Url = new Uri("https://github.com/s2kdesign-com/CoinGardenWorld/blob/main/LICENSE.txt")
+                    }
+                });
+                // Add Swagger Signalr
+                // TODO: Is there a SIGNALR connection here?
+                // options.AddSignalRSwaggerGen();
+
+
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+
+
+                var microsoftIdentityOptions = new MicrosoftIdentityOptions();
+                Configuration.GetSection("AzureAd").Bind(microsoftIdentityOptions);
+                // Enabled OAuth security in Swagger
+                var scopesConfig = Configuration.GetValue<string>("AzureAd:Scopes").Split(' ');
+                var scopes = new Dictionary<string, string>();
+                foreach (var scope in scopesConfig)
+                {
+                    scopes.Add($"https://{microsoftIdentityOptions.Domain}/{microsoftIdentityOptions.ClientId}/{scope}", "");
+
+                }
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement() {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "oauth2"
+                },
+                Scheme = "oauth2",
+                Name = "oauth2",
+
+                In = ParameterLocation.Header
+            },
+            new List <string> ()
+        }
+    });
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.OAuth2,
+                    Flows = new OpenApiOAuthFlows
+                    {
+                        Implicit = new OpenApiOAuthFlow()
+                        {
+                            AuthorizationUrl = new Uri($"{microsoftIdentityOptions.Instance}/{microsoftIdentityOptions.Domain}/{microsoftIdentityOptions.SignUpSignInPolicyId}/oauth2/v2.0/authorize"),
+                            TokenUrl = new Uri($"{microsoftIdentityOptions.Instance}/{microsoftIdentityOptions.Domain}/{microsoftIdentityOptions.SignUpSignInPolicyId}/oauth2/v2.0/token"),
+                            Scopes = scopes,
+
+                        }
+                    },
+                });
+
+            });
+            #endregion
+
             services.AddBotRuntime(Configuration);
         }
 
@@ -57,6 +139,19 @@ namespace CoinGardenWorldBot_Api
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                //Localhost-CoinGarden-Mobile-Web-SwaggerUI
+                options.OAuthAppName("Swagger Client");
+                options.OAuthClientId(Configuration.GetValue<string>("SwaggerUI:ClientId"));
+                options.OAuthClientSecret(Configuration.GetValue<string>("SwaggerUI:ClientSecret"));
+                options.OAuthUseBasicAuthenticationWithAccessCodeGrant();
+                //options.OAuth2RedirectUrl("https://localhost:8002/swagger/oauth2-redirect.html");
+                //options.OAuthUsePkce();
+                //options.RoutePrefix = string.Empty;
+
+            });
             app.UseDefaultFiles();
 
             // Set up custom content types - associating file extension to MIME type.
