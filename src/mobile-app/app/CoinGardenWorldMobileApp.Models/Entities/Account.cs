60e5c150c26496b55ca4e9e2d25182c60eb62222
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2023-11-15 23:31:55 +0200

    #7 So , when there is no good mapping in some entity , httpclient is returning stream error

--- src/mobile-app/app/CoinGardenWorldMobileApp.Models/Entities/Account.cs
+++ src/mobile-app/app/CoinGardenWorldMobileApp.Models/Entities/Account.cs
@@ -13,8 +13,8 @@ namespace CoinGardenWorldMobileApp.Models.Entities
         // {1} = Max Length
         // {2} = Min Length
         [StringLength(150, ErrorMessage = "{0} can have a max of {1} characters")]
-        public string Email { get; set; }
-
+        public string Email { get; set; } = "";
+         
         [StringLength(150, ErrorMessage = "{0} can have a max of {1} characters")]
         public string? Username { get; set; }
 
@@ -24,11 +24,11 @@ namespace CoinGardenWorldMobileApp.Models.Entities
         [StringLength(500, ErrorMessage = "{0} can have a max of {1} characters")]
         public string? ProfileIntroduction { get; set; }
 
-        public BlobImage? ProfilePicture { get; set; } 
+        public BlobImage? ProfilePicture { get; set; } = new();
 
-        public List<AccountBadge>? Badges { get; set; } 
+        public List<AccountBadge>? Badges { get; set; } = new();
 
-        public List<AccountRole>? Roles { get; set; } 
+        public List<AccountRole>? Roles { get; set; } = new();
 
         [IgnoreOnList]
         public List<AccountExternalLogins>? ExternalLogins { get; set; } = new();
